6cf1993925f70abb8fee2fb381a3d1477a1b1106
SimonCropp <simon.cropp@gmail.com>
2020-06-30 21:00:05 +1000

    refs

--- src/Verify.ImageSharp/VerifyImageSharp.cs
+++ src/Verify.ImageSharp/VerifyImageSharp.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Bmp;
@@ -13,14 +12,14 @@ namespace VerifyTests
     {
         public static void Initialize()
         {
-            VerifierSettings.RegisterFileConverter("bmp", "bmp", ConvertBmp);
-            VerifierSettings.RegisterFileConverter("gif", "gif", ConvertGif);
-            VerifierSettings.RegisterFileConverter("jpg", "jpg", ConvertJpg);
-            VerifierSettings.RegisterFileConverter("png", "png", ConvertPng);
-            VerifierSettings.RegisterFileConverter("bmp", ConvertBmpImage, IsImage);
-            VerifierSettings.RegisterFileConverter("gif", ConvertGifImage, IsImage);
-            VerifierSettings.RegisterFileConverter("jpg", ConvertJpgImage, IsImage);
-            VerifierSettings.RegisterFileConverter("png", ConvertPngImage, IsImage);
+            VerifierSettings.RegisterFileConverter("bmp", ConvertBmp);
+            VerifierSettings.RegisterFileConverter("gif", ConvertGif);
+            VerifierSettings.RegisterFileConverter("jpg", ConvertJpg);
+            VerifierSettings.RegisterFileConverter("png", ConvertPng);
+            VerifierSettings.RegisterFileConverter(ConvertBmpImage, IsImage);
+            VerifierSettings.RegisterFileConverter(ConvertGifImage, IsImage);
+            VerifierSettings.RegisterFileConverter(ConvertJpgImage, IsImage);
+            VerifierSettings.RegisterFileConverter(ConvertPngImage, IsImage);
         }
 
         private static bool IsImage(object target)
@@ -30,59 +29,59 @@ namespace VerifyTests
 
         static ConversionResult ConvertBmpImage(object image, VerifySettings settings)
         {
-            return Convert((Image) image, new BmpEncoder());
+            return Convert((Image) image, "bmp", new BmpEncoder());
         }
 
         static ConversionResult ConvertGifImage(object image, VerifySettings settings)
         {
-            return Convert((Image) image, new GifEncoder());
+            return Convert((Image) image, "gif", new GifEncoder());
         }
 
         static ConversionResult ConvertJpgImage(object image, VerifySettings settings)
         {
-            return Convert((Image) image, new JpegEncoder());
+            return Convert((Image) image, "jpg", new JpegEncoder());
         }
 
         static ConversionResult ConvertPngImage(object image, VerifySettings settings)
         {
-            return Convert((Image) image, new PngEncoder());
+            return Convert((Image) image, "png", new PngEncoder());
         }
 
-        static ConversionResult Convert(Image image, IImageEncoder encoder)
+        static ConversionResult Convert(Image image, string extension, IImageEncoder encoder)
         {
             var stream = new MemoryStream();
             var info = image.GetInfo();
             image.Save(stream, encoder);
             stream.Position = 0;
-            return new ConversionResult(info, new List<Stream> {stream});
+            return new ConversionResult(info, extension, stream);
         }
 
         static ConversionResult ConvertBmp(Stream stream, VerifySettings settings)
         {
-            return Convert(stream, new BmpDecoder());
+            return Convert(stream, "bmp", new BmpDecoder());
         }
 
         static ConversionResult ConvertGif(Stream stream, VerifySettings settings)
         {
-            return Convert(stream, new GifDecoder());
+            return Convert(stream, "gif", new GifDecoder());
         }
 
         static ConversionResult ConvertJpg(Stream stream, VerifySettings settings)
         {
-            return Convert(stream, new JpegDecoder());
+            return Convert(stream, "jpg", new JpegDecoder());
         }
 
         static ConversionResult ConvertPng(Stream stream, VerifySettings settings)
         {
-            return Convert(stream, new PngDecoder());
+            return Convert(stream, "png", new PngDecoder());
         }
 
-        static ConversionResult Convert(Stream stream, IImageDecoder decoder)
+        static ConversionResult Convert(Stream stream, string extension, IImageDecoder decoder)
         {
             var image = Image.Load(stream, decoder);
             stream.Position = 0;
             var info = image.GetInfo();
-            return new ConversionResult(info, new List<Stream> {stream});
+            return new ConversionResult(info, extension, stream);
         }
     }
 }
