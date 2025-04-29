1c97b62d7661639226dce070f3cccebf775ad109
Simon Cropp <simon.cropp@gmail.com>
2025-04-29 21:32:22 +1000

    Respect target name when converting (#655)

---
+++ src/Verify.ImageSharp/GlobalUsings.cs
@@ -0,0 +1,9 @@
+// Global using directives
+
+global using SixLabors.ImageSharp;
+global using SixLabors.ImageSharp.Formats;
+global using SixLabors.ImageSharp.Formats.Bmp;
+global using SixLabors.ImageSharp.Formats.Gif;
+global using SixLabors.ImageSharp.Formats.Jpeg;
+global using SixLabors.ImageSharp.Formats.Png;
+global using SixLabors.ImageSharp.Formats.Tiff;
